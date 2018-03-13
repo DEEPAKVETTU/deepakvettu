1baea7f5fa3bc826eb96a212521dda834aee7aae
DEEPAKVETTU <36249863+DEEPAKVETTU@users.noreply.github.com>
2018-03-13 19:51:44 +0530

    Create ex24.c

---
+++ ex24.c
@@ -0,0 +1,36 @@
+  #include <stdio.h>
+    void main()
+    {
+ 
+        int i, j, b, n, number[30];
+        printf("Enter the value of N \n");
+        scanf("%d", &n);
+ 
+        printf("Enter the numbers \n");
+        for (i = 0; i < n; ++i)
+            scanf("%d", &number[i]);
+ 
+        for (i = 0; i < n; ++i) 
+        {
+ 
+            for (j = i + 1; j < n; ++j)
+            {
+ 
+                if (number[i] > number[j]) 
+                {
+ 
+                    b =  number[i];
+                    number[i] = number[j];
+                    number[j] = b;
+ 
+                }
+ 
+            }
+ 
+        }
+ 
+        printf("The numbers arranged in ascending order are given below \n");
+        for (i = 0; i < n; ++i)
+            printf("%d\n", number[i]);
+ 
+    }
