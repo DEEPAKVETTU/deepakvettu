ac77430dd80e307bb8a857e96cfcd3a0e2654113
DEEPAKVETTU <36249863+DEEPAKVETTU@users.noreply.github.com>
2018-03-13 07:18:22 +0530

    Update ex9.c

--- ex9.c
+++ ex9.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 void main()
 {
-int i,j,k,n,j=0,a[n];
-scan("&n&k",n,k);
-for(i=1;i<=n;i++)
+int i,j,k,m,j=0,a[n];
+scan("&n&k",m,k);
+for(i=1;i<=m;i++)
 scanf("%d",&a[i]);
 for(i=1;i<k;i++)
 j=j+k;
