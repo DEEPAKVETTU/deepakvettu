c07617317c76ba74b3c78a74b0056a50c3baf646
DEEPAKVETTU <36249863+DEEPAKVETTU@users.noreply.github.com>
2018-03-14 06:21:52 +0530

    Create ex40.c

---
+++ ex40.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a=0,b=1,d,i,l;
+    printf("the length of fibonacci series is");
+    scanf("%d",&l);
+    for(i=0;i<l;i++)
+    {
+        printf("%d",a);
+        d=a+b;
+        a=b;
+        b=c;
+        
+    }
+    }
