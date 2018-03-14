e1b858af6274cdcb10cfe4bd999c641579c4b6fc
DEEPAKVETTU <36249863+DEEPAKVETTU@users.noreply.github.com>
2018-03-14 06:04:27 +0530

    Create ex30.c

---
+++ ex30.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+int h,h1=5,h2=4,m,n1=50,n2=40;
+printf("\n enter the hours and min");
+scanf("%d%d%d%d",&h1,&h2,&n1,&n2);
+h=h1-h2;
+m=n1-n2;
+printf("%d\t%d",h,m);
+}
