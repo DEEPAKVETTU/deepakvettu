75d068c0d3d088d3c6e74393d0b7252369559ebb
DEEPAKVETTU <36249863+DEEPAKVETTU@users.noreply.github.com>
2018-03-14 05:57:21 +0530

    Update ex25.c

--- ex25.c
+++ ex25.c
@@ -3,19 +3,19 @@
  
 void main()
 {
-    int i,n;
+    int i,m;
     int sum=0,a[60];
     int avg=0;
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
+    scanf("%d",&m);
+    for(i=0;i<m;i++)
     {
         scanf("%d",&a[i]);
     }
-    for(i=0;i<n;i++)
+    for(i=0;i<m;i++)
     {
         sum=sum+a[i];
     }
-avg=sum/n;
+avg=sum/m;
 printf("%d",avg);
     
 }
