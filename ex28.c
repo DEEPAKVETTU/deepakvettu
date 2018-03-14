2db12be4ed5e91f3cf1a7148cea068d9af9e5ba6
DEEPAKVETTU <36249863+DEEPAKVETTU@users.noreply.github.com>
2018-03-14 06:01:09 +0530

    Create ex28.c

---
+++ ex28.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int a[10],i,j,m=5;
+ for(i=0;i<m;i++)
+   {
+     scanf("%d",&a[i]); 
+     printf("%d\t%d\n",a[i],i);
+     getch();
+   }
