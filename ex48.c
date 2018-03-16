5d59241f074339c0e89b227726c97cef51be3957
DEEPAKVETTU <36249863+DEEPAKVETTU@users.noreply.github.com>
2018-03-16 05:55:57 +0530

    Rename ex47.c to ex48.c

---
+++ ex48.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int a,b,array[50],s=0,av;
+	printf("enter the size of the array");
+	scanf("%d",&b);
+	printf("enter the array elements");
+	for(a=0;a<n;a++)
+	{
+		scanf("%d",&array[a]);
+	
+	}
+	for(a=0;a<n;a++)
+	{
+		sum=sum+array[a];
+	}
+	avg=sum/b;
+	printf("\n%d",s);
+	printf("\n%d",av);
+	return 0;
+}
