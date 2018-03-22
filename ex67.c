ee6548fa095ba092c4a24951127cdef78be14c83
DEEPAKVETTU <36249863+DEEPAKVETTU@users.noreply.github.com>
2018-03-22 06:06:42 +0530

    Update ex67.c

--- ex67.c
+++ ex67.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 void main()
 {
-  int a,b,c;
+  int a,b,c,d;
   printf("enter the number:");
   scanf("%d",&a);
   b=a%10;
-  c=b+a;
-  printf("answer %d",c);
+  c=10-b;
+  d=c+a;
+  printf("answer %d",d);
 }
