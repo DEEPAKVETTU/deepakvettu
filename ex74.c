6285535996a430ff4a7b8afea3885aacd4685ac9
DEEPAKVETTU <36249863+DEEPAKVETTU@users.noreply.github.com>
2018-04-03 06:16:59 +0530

    Create ex74.c

---
+++ ex74.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+ int main()
+{
+       float p;
+       printf("Enter the float digit");
+       scanf("%f",&p);
+       printf("round of  %f is  %f\n", p, round(p));
+       return 0;
+}
