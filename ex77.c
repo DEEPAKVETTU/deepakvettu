23eaeb9e217052b319708c7e5ff68316d6720e7f
DEEPAKVETTU <36249863+DEEPAKVETTU@users.noreply.github.com>
2018-04-04 06:14:18 +0530

    Update ex77.c

--- ex77.c
+++ ex77.c
@@ -1,16 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 void main()
 {
-    int d,p,c=0;
-    printf ("Enter a number \n");
-    scanf ("%d",&p);
-    for (d=1;d<=p;d++)
-    {
-        if(p%d==0)
-        c=c+1;
-    }
-    if (c==2)
-    printf ("The number is PRIME");
-    else
-    printf ("The number is COMPOSITE");
+int d,p;
+scanf("%d\n",&p);
+printf("%d\n",p);
+for(d=1;d<=p;d++)
+{
+if(p%d==0)
+{
+printf("%d\n",d);
+}
+}
+getch();
 }
