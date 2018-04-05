1953eacd78267ae28132511e0266359c7e1c776c
DEEPAKVETTU <36249863+DEEPAKVETTU@users.noreply.github.com>
2018-04-05 06:06:38 +0530

    Rename exp8.py to exp9.py

---
+++ exp9.py
@@ -0,0 +1,9 @@
+days=["Monday","Tuesday","Wednesday","Thursday","Friday"]
+day=["Sunday","Saturday"]
+p=input("Enter the day:")
+for i in range(5):
+    if(p==days[i]):
+            print("no")
+for i in range(2):
+    if(p==day[i]):
+        print("yes")
