dfa2c015b982fa40ccea96e508dd4912471adbbc
DEEPAKVETTU <36249863+DEEPAKVETTU@users.noreply.github.com>
2018-05-25 11:10:56 +0530

    Update expro21.py

--- expro21.py
+++ expro21.py
@@ -1,15 +1,18 @@
 a=input("Enter the numbers")
-b=a[:len(a)//2]
-c=a[len(a)//2:]
-d=[]
-e=[]
+b=a[len(a)//2:]
+c=a[:len(a)//2]
+d=0
+e=0
 for i in b:
   d=d+int(i)
+
 for j in c:
-  e=e+int(i)
+  e=e+int(j)
+  
 f=d/len(b)
 g=e/len(c)
+
 if f==g:
   print("yes")
 else:
-  print("no")
+ print("no")
