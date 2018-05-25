091700c61a0b2b6e4549768017602e46b0400e4a
DEEPAKVETTU <36249863+DEEPAKVETTU@users.noreply.github.com>
2018-05-25 10:27:04 +0530

    Create expro53.py

---
+++ expro53.py
@@ -0,0 +1,11 @@
+a=input("enter the string")
+n=0
+k=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+a=a.lower()
+for i in a:
+  if i in k:
+    n=n+1
+if n==26:
+  print("yes")
+else:
+ print("no")
